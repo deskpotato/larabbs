537507c38a18bf64117087d8dfbb9481c2195c8f
miduo Yu <2225122776@qq.com>
2019-05-13 07:31:46 +0000

    注册验证码

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -60,6 +60,20 @@
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
                             </div>
                         </div>
+                        {{--  验证码  --}}
+                        <div class="form-group row">
+                            <label for="captcha" class="col-md-4 col-form-label text-md-right">验证码</label>
+                            <div class="col-md-6">
+                                <input id="captcha" class="form-control{{ $errors->has('captcha') ? ' is-invalid' : '' }}" name='captcha'/>
+                                <img class="thumbnail captcha mt-3 mb-2" src="{{ captcha_src('flat') }}" onclick="this.src=this.src+'?'+Math.random()" />
+                                @if ($errors->has('captcha'))
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $errors->first('captcha') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                         </div>
+
 
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">

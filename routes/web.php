6d946db7c52732ebe62cb950b929c75445b5efbf
miduo Yu <2225122776@qq.com>
2019-05-14 06:22:09 +0000

    生成话题topic骨架

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,5 @@ Route::get('/', 'PagesController@root')->name('index');
 Auth::routes(['verify'=>true]);
 
 Route::resource('users','UsersController',['only'=>['show','edit','update']]);
+
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);

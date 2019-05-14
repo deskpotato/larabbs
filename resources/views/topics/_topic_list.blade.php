337e292c4852fe6009a364bc262f347aa4fe89e8
miduo Yu <2225122776@qq.com>
2019-05-14 07:44:15 +0000

    分类话题列表

--- resources/views/topics/_topic_list.blade.php
+++ resources/views/topics/_topic_list.blade.php
@@ -17,7 +17,7 @@
         </a>
     </div>
     <small class="media-body meta text-secondary">
-        <a class="text-secondary" href="#" title="{{ $topic->category->name }}">
+        <a class="text-secondary" href="{{ route('categories.show',$topic->category_id) }}" title="{{ $topic->category->name }}">
             <i class="far fa-folder"></i>
             {{ $topic->category->name }}
         </a>
@@ -32,7 +32,7 @@
     </small>
     </div>
     </li>
-    @if ( ! $loop->last)
+    @if (!$loop->last)
         <hr>
     @endif
     @endforeach

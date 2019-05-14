9823853a2fc30a4ca8a84e5ba8c8d141952de9be
miduo Yu <2225122776@qq.com>
2019-05-14 06:49:10 +0000

    填充用户和话题数据

--- database/factories/TopicFactory.php
+++ database/factories/TopicFactory.php
@@ -3,7 +3,13 @@
 use Faker\Generator as Faker;
 
 $factory->define(App\Models\Topic::class, function (Faker $faker) {
+    $sentence  =$faker->sentence();
+    $updated_at = $faker->dateTimeThisMonth();
+    $ceated_at = $faker->dateTimeThisMonth($updated_at);
     return [
-        // 'name' => $faker->name,
+        'title'=>$sentence,
+        'body'=>$faker->text(),
+        'created_at'=>$ceated_at,
+        'updated_at'=>$updated_at,
     ];
 });

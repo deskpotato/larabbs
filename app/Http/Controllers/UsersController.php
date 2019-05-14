d207b57a78163a05e2bad649e3975ab0e416d5ef
miduo Yu <2225122776@qq.com>
2019-05-14 03:28:28 +0000

    裁剪头像+授权访问

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -9,7 +9,11 @@ use App\Handlers\ImageUploadHandler;
 
 class UsersController extends Controller
 {
-    
+    public function __construct()
+    {
+        $this->middleware('auth',['except'=>['show']]);
+    }
+
     public function show(User $user)
     {
         return view('users.show',compact('user'));
@@ -17,15 +21,17 @@ class UsersController extends Controller
 
     public function edit(User $user)
     {
+        $this->authorize('update',$user);
         return view('users.edit',compact('user'));
 
     }
 
     public function update(UserRequest $request, User $user)
     {
+        $this->authorize('update',$user);
         $data = $request->all();
         if($request->avatar){
-            $result = ImageUploadHandler::save($request->avatar,'avatar',$user->id);
+            $result = ImageUploadHandler::save($request->avatar,'avatar',$user->id,250);
             if($result){
                 $data['avatar'] = $result['path'];
             }

e47ba68ac10900fca02557b0596d0282122093c5
miduo Yu <2225122776@qq.com>
2019-05-14 02:48:46 +0000

    上传头像

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 use App\Http\Requests\UserRequest;
+use App\Handlers\ImageUploadHandler;
 
 class UsersController extends Controller
 {
@@ -22,7 +23,15 @@ class UsersController extends Controller
 
     public function update(UserRequest $request, User $user)
     {
-        $user->update($request->all());
+        $data = $request->all();
+        if($request->avatar){
+            $result = ImageUploadHandler::save($request->avatar,'avatar',$user->id);
+            if($result){
+                $data['avatar'] = $result['path'];
+            }
+        }
+
+        $user->update($data);
         return redirect()->route('users.show',$user->id)->with('success','个人资料更成功!!');
         
     }

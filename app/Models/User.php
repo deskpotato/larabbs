b677518aea9fbdf84f28bb310b22dccd66eeefc6
miduo Yu <2225122776@qq.com>
2019-05-15 06:03:07 +0000

    回复数据

--- app/Models/User.php
+++ app/Models/User.php
@@ -42,4 +42,9 @@ class User extends Authenticatable implements MustVerifyEmail
     {
         return $this->hasMany(Topic::class);    
     }
+
+    public function replies()
+    {
+        return $this->hasMany(Reply::class);    
+    }
 }

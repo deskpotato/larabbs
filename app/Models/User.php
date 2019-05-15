263cf3bde7969fb9a6ff27b978935eca55fa0e31
miduo Yu <2225122776@qq.com>
2019-05-15 07:24:39 +0000

    消息通知列表

--- app/Models/User.php
+++ app/Models/User.php
@@ -29,6 +29,14 @@ class User extends Authenticatable implements MustVerifyEmail
 
     }
 
+    //标记通知为已读
+
+    public function markAsRead(){
+        $this->notification_count = 0;
+        $this->save();
+        $this->unreadNotifications->markAsRead();
+    }
+
 
     /**
      * The attributes that are mass assignable.
